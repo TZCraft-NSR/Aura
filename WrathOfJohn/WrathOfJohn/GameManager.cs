bb6da00de0e505b7f94988174f09616d2220837c
thakyZ <thakyz@outlook.com>
2015-02-06 19:21:45 -0800

    Fixed player animations

--- WrathOfJohn/WrathOfJohn/GameManager.cs
+++ WrathOfJohn/WrathOfJohn/GameManager.cs
@@ -17,28 +17,60 @@ namespace WrathOfJohn
     /// </summary>
 	public class GameManager : Microsoft.Xna.Framework.DrawableGameComponent
 	{
-        public SpriteBatch spriteBatch;
-		public Game1 myGame;
+		/// <summary>
+		/// The spriteBatch to draw with.
+		/// </summary>
+        SpriteBatch spriteBatch;
+		/// <summary>
+		/// The game that the GameManager runs off of.
+		/// </summary>
+		Game1 myGame;
 
-        public List<Sprite.AnimationSet> npcAnimationSetList;
-        public List<Vector2> npcPositions;
-        public List<Sprite.AnimationSet> tempAnimationSetList = new List<Sprite.AnimationSet>();
+		/// <summary>
+		/// The animation list for the player.
+		/// </summary>
+        List<Sprite.AnimationSet> tempPlayerAnimationSetList = new List<Sprite.AnimationSet>();
 
-        public Player player;
-        public Texture2D playerTexture;
-        public Texture2D debugDotTexture;
+		/// <summary>
+		/// The player class.
+		/// </summary>
+        Player player;
+		/// <summary>
+		/// The player class' texture.
+		/// </summary>
+        Texture2D playerTexture;
+		/// <summary>
+		/// This is the dot texture to test bounding boxes.
+		/// </summary>
+		Texture2D debugDotTexture;
+		/// <summary>
+		/// To debug variables.
+		/// </summary>
+		Label debugLabel;
 
+		/// <summary>
+		/// This is to create the Game Manager.
+		/// </summary>
+		/// <param name="game">This is for the Game stuff</param>
         public GameManager(Game1 game) : base(game)
 		{
 			myGame = game;
+
+			// This is to fix the Initalize() function.
 			this.Initialize();
 		}
 
+		/// <summary>
+		/// This is to initalize the Game Manager
+		/// </summary>
         public override void Initialize()
         {
             base.Initialize();
         }
 
+		/// <summary>
+		/// This is to load the Game Manager's sprites and classes.
+		/// </summary>
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(Game.GraphicsDevice);
@@ -46,28 +78,43 @@ namespace WrathOfJohn
             playerTexture = Game.Content.Load<Texture2D>(@"Images\players\player");
             debugDotTexture = Game.Content.Load<Texture2D>(@"Images\debugStuff\line");
 
-            tempAnimationSetList.Add(new Sprite.AnimationSet("IDLE", playerTexture, new Point(60, 50), new Point(1, 1), new Point(0, 0), 1000));
-            tempAnimationSetList.Add(new Sprite.AnimationSet("WALK", playerTexture, new Point(60, 50), new Point(4, 3), new Point(0, 0), 50));
-            tempAnimationSetList.Add(new Sprite.AnimationSet("JUMP", playerTexture, new Point(60, 50), new Point(4, 1), new Point(0, 150), 100));
+            tempPlayerAnimationSetList.Add(new Sprite.AnimationSet("IDLE", playerTexture, new Point(60, 50), new Point(1, 1), new Point(0, 0), 1000));
+            tempPlayerAnimationSetList.Add(new Sprite.AnimationSet("WALK", playerTexture, new Point(60, 50), new Point(4, 3), new Point(0, 0), 50));
+            tempPlayerAnimationSetList.Add(new Sprite.AnimationSet("JUMP", playerTexture, new Point(60, 50), new Point(4, 1), new Point(0, 150), 100));
 
-            player = new Player(playerTexture, new Vector2((myGame.windowSize.X - 60) / 2, (myGame.windowSize.Y - 50) / 2), myGame, Keys.A, Keys.D, Keys.Space, 2f, tempAnimationSetList);
+            player = new Player(playerTexture, new Vector2((myGame.windowSize.X - 60) / 2, (myGame.windowSize.Y - 50) / 2), myGame, Keys.A, Keys.D, Keys.Space, 2f, tempPlayerAnimationSetList);
+
+			debugLabel = new Label(Vector2.Zero, myGame.debugFont, 1f, Color.Black, "");
 
             base.LoadContent();
         }
 
+		/// <summary>
+		/// This is to update the Game Manager's Classes and sprites.
+		/// </summary>
+		/// <param name="gameTime">This is to check the run time.</param>
         public override void Update(GameTime gameTime)
         {
             player.Update(gameTime);
+			debugLabel.Update(gameTime, "isGrounded=" + player.isGrounded + " isJumping=" + player.isJumping + " isFalling=" + player.isFalling + " isWalking=" + player.isWalking + " bleedOff=" + player.BleedOff);
 
             base.Update(gameTime);
         }
 
+		/// <summary>
+		/// This is to draw the Game Manager's Classes and Objects
+		/// </summary>
+		/// <param name="gameTime"><This is to check the run time./param>
         public override void Draw(GameTime gameTime)
         {
             spriteBatch.Begin();
                 player.Draw(gameTime, spriteBatch);
+				// Debug bounding boxes.
                 spriteBatch.Draw(debugDotTexture, new Rectangle(0, ((int)(myGame.windowSize.Y - 50) / 2) + player.currentAnimation.frameSize.Y, (int)myGame.windowSize.X, 1), Color.White);
+				// Debug bounding boxes.
                 spriteBatch.Draw(debugDotTexture, new Rectangle((int)player.position.X - 10, ((int)player.position.Y - 1) + player.currentAnimation.frameSize.Y, player.currentAnimation.frameSize.X + 10, 1), Color.White);
+				// To debug variables.
+				debugLabel.Draw(gameTime, spriteBatch);
             spriteBatch.End();
 
             base.Draw(gameTime);
